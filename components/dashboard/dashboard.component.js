257036d105390cf3ddc0d9a97e7a49992756c7f5
Jose Tejada <65978779+tejada3@users.noreply.github.com>
2021-08-25 18:18:51 -0400

    dashboard

--- components/dashboard/dashboard.component.js
+++ components/dashboard/dashboard.component.js
@@ -1,4 +1,5 @@
 import { ViewComponent } from '../view.component.js';
+import env from '../../util/env.js';
 import state from '../../util/state.js';
 import router from '../../app.js';
 
@@ -6,6 +7,57 @@ DashboardComponent.prototype = new ViewComponent('dashboard');
 function DashboardComponent() {
 
     let welcomeUserElement;
+    let tableElement;
+    let button;
+
+    function displayCourses(...courseList) {
+        // if (tableElement.hasChildNodes()) {
+        //     tableElement.innerHTML = "";
+        //     return;
+        // }
+
+        for (let item of courseList) {
+            let tr = document.createElement("tr");
+            let courseCode = document.createElement("td");
+            let courseName = document.createElement("td");
+            let startDate = document.createElement("td");
+            let endDate = document.createElement("td");
+
+            courseCode.innerText = item.course_code;
+            courseName.innerText = item.course_name;
+            startDate.innerText = item.start_date;
+            endDate.innerText = item.end_date;
+
+            tr.appendChild(courseCode);
+            tr.appendChild(courseName);
+            tr.appendChild(startDate);
+            tr.appendChild(endDate);
+
+            tableElement.append(tr);
+        }
+    }
+
+    async function getCourses() {
+        try {
+            let response = await fetch(`${env.apiUrl}/course`, {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify()
+            });
+
+            let data = await response.json();
+
+            renderCourses(data);
+        } catch (e) {
+            console.log(e);
+        }
+    }
+
+    function renderCourses(payload) {
+        displayCourses(...payload);
+    }
 
     this.render = function() {
 
@@ -22,7 +74,11 @@ function DashboardComponent() {
         DashboardComponent.prototype.injectTemplate(() => {
 
             welcomeUserElement = document.getElementById('welcome-user');
+            tableElement = document.getElementById("table-body");
+            button = document.getElementById("my-button");
+
             welcomeUserElement.innerText = currentUsername;
+            button.addEventListener("contact-tab", getCourses);
         });
 
     }
